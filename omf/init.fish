bc532c8599c15dcbb0227c6b8afee2dc67dcec85
Martin Chuckeles <martin@jakubik.info>
2016-03-24 18:02:59 +0100

    Add aliases

--- omf/init.fish
+++ omf/init.fish
@@ -1,3 +1,5 @@
+## SETTINGS
+
 # editors, pagers
 set -gx BROWSER open
 set -gx EDITOR vim
@@ -6,4 +8,25 @@ set -gx MANPAGER $PAGER
 
 # fish greeting
 set -gx fish_greeting ""
+
+## ALIASES
+
+# navigation
+alias .. "cd .."
+alias ... "cd ../.."
+alias .... "cd ../../.."
+
+# ls
+alias l "ls -F"
+alias la "ls -AF"
+alias ll "ls -lF"
+alias lla "ls -AlF"
+alias lal lla
+
+# apps
+alias bo bower
+alias bu bundler
+alias g git
+alias gu gulp
+alias r rails
 
