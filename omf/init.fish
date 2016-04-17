611c6e3a9851b3d49c5c52e45e9d0637b3edc4df
Martin Chuckeles <martin@jakubik.info>
2016-04-17 22:54:27 +0200

    Add aliases for node and npm

--- omf/init.fish
+++ omf/init.fish
@@ -12,23 +12,25 @@ set -gx fish_greeting ""
 ## ALIASES
 
 # navigation
-alias .. "cd .."
-alias ... "cd ../.."
+alias ..   "cd .."
+alias ...  "cd ../.."
 alias .... "cd ../../.."
 
 # ls
-alias l "ls -F"
-alias la "ls -AF"
-alias ll "ls -lF"
+alias l   "ls -F"
+alias la  "ls -AF"
+alias ll  "ls -lF"
 alias lla "ls -AlF"
 alias lal lla
 
 # apps
 alias bo bower
 alias bu bundler
-alias g git
+alias g  git
 alias gu gulp
-alias r rails
+alias no node
+alias np npm
+alias r  rails
 
 ## RBENV
 
