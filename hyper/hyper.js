02c4a59028dc3af5b164e902508dbfdd3068368d
Martin Jakubik <martin.jakubik@exponea.com>
2019-10-22 15:31:54 +0200

    add hyper config

---
+++ hyper/hyper.js
@@ -0,0 +1,53 @@
+// see https://hyper.is#cfg for all supported options
+module.exports = {
+  config: {
+    updateChannel: 'stable',
+
+    fontSize: 16,
+    fontFamily: '"Dank Mono", "Roboto Mono for Powerline", monospace',
+    fontWeight: 'normal',
+    fontWeightBold: 'bold',
+    lineHeight: 1.2,
+
+    cursorShape: 'BLOCK',
+    cursorBlink: false,
+
+    padding: '12px 0',
+
+    shell: '/usr/local/bin/fish',
+    shellArgs: ['--login'],
+
+    bell: 'SOUND',
+    copyOnSelect: false,
+    defaultSSHApp: true,
+
+    // hyper-pane configuration
+    paneNavigation: {
+      hotkeys: {
+        jump_prefix: 'command+option',
+        maximize: 'command+enter',
+        navigation: {
+          up: 'command+option+up',
+          left: 'command+option+left',
+          right: 'command+option+right',
+          down: 'command+option+down'
+        }
+      }
+    }
+  },
+
+  plugins: ["hyper-solarized-light", "hypercwd", "hyper-pane"],
+
+  keymaps: {
+    'tab:next': [
+      'command+shift+]',
+      'command+shift+right',
+      'ctrl+tab'
+    ],
+    'tab:prev': [
+      'command+shift+[',
+      'command+shift+left',
+      'ctrl+shift+tab'
+    ],
+  },
+};
