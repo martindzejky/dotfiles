ebf8f29072a7736e3f487d249b258ee6e8e507a0
Martin Jakubik <martin.jakubik@exponea.com>
2020-03-16 21:24:53 +0100

    switch from z to zoxide

--- omf/init.fish
+++ omf/init.fish
@@ -38,6 +38,19 @@ alias y  yarn
 
 # PATH
 
+# zoxide
+if which zoxide > /dev/null
+    # initialize zoxide
+    zoxide init fish | source
+
+    # migrate .z database if present
+    if test -e ~/.z
+        echo "migrating z database to zoxide"
+        zoxide migrate --merge ~/.z
+        rm -r ~/.z
+    end
+end
+
 # node/nvm
 if test -d ~/.nvm
     set -gx NVM_DIR ~/.nvm
