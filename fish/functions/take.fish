156043a453cf404e5348fda89fefe4ab97eb200d
Martin Jakubik <martin.jakubik@exponea.com>
2020-03-29 17:46:18 +0200

    add a take function
    inspired by
    https://twitter.com/anniebombanie_/status/1244109609952063488

---
+++ fish/functions/take.fish
@@ -0,0 +1,14 @@
+function take --description="Create a directory and cd to it"
+    command mkdir $argv
+
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+
+            case '*'
+                cd $argv[(count $argv)]
+                return
+        end
+    end
+end
+
