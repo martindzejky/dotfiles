c2a8a82b0d445346c84047f56ba6a939a4f15c03
Martin Jakubik <martin@jakubik.info>
2020-12-03 12:13:28 +0100

    make gsw handle remote branches

--- fish/functions/gsw.fish
+++ fish/functions/gsw.fish
@@ -21,6 +21,18 @@ function gsw \
             return
     end
 
+    # ensure that only one branch was selected
+    if test (count $selected) -gt 1
+        echo "select only one branch"
+        return 1
+    end
+
+    # if a remote branch has been selected, check out its local equivalent
+    if string match --regex --quiet '^origin/' $selected
+        set selected (string split --max 1 "/" $selected)
+        set selected $selected[2]
+    end
+
     git switch $selected
 end
 
