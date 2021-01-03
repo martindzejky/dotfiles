283bce6d0caae4722ce5a965872c2c683cafe993
Martin Jakubik <martin@jakubik.info>
2021-01-03 20:12:32 +0100

    make is-os-dark-mode faster and add a copy for light mode

--- fish/functions/is-os-dark-mode.fish
+++ fish/functions/is-os-dark-mode.fish
@@ -1,5 +1,10 @@
-function is-os-dark-mode -d "Checks whether the OS is in dark mode"
-    set -l result (osascript -l JavaScript -e 'Application("System Events").appearancePreferences.darkMode()')
-    return (test "$result" = "true")
+function is-os-dark-mode \
+    -d "Checks whether the OS is in dark mode"
+
+    set -l result (
+        defaults read -g AppleInterfaceStyle 2>/dev/null # prints nothing or "Dark"
+    )
+
+    return (test "$result" = "Dark")
 end
 
---
+++ fish/functions/is-os-light-mode.fish
@@ -0,0 +1,6 @@
+function is-os-light-mode \
+    -d "Checks whether the OS is in light mode"
+
+    return (not is-os-dark-mode)
+end
+
