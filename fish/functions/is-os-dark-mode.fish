b4656042b815c28770e653b0d43677aba762cf63
Martin Jakubik <martin@jakubik.info>
2021-01-12 09:12:43 +0100

    switch is-os-dark-mode function to the old implementation
    Use this again:
    ```
    osascript -l JavaScript -e 'Application("System Events").appearancePreferences.darkMode()'
    ```
    
    instead of:
    ```
    defaults read -g AppleInterfaceStyle
    ```
    
    because the latter incorrectly reports "Dark" even when the OS is
    already in light mode.

--- fish/functions/is-os-dark-mode.fish
+++ fish/functions/is-os-dark-mode.fish
@@ -2,9 +2,9 @@ function is-os-dark-mode \
     -d "Checks whether the OS is in dark mode"
 
     set -l result (
-        defaults read -g AppleInterfaceStyle 2>/dev/null # prints nothing or "Dark"
+        osascript -l JavaScript -e 'Application("System Events").appearancePreferences.darkMode()'
     )
 
-    return (test "$result" = "Dark")
+    return (test "$result" = "true")
 end
 
