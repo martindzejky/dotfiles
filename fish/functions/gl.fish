dbea0ac6eb0ecfdeee3af5a27e728558076aa04b
Martin Jakubik <martin@jakubik.info>
2021-03-02 23:50:44 +0100

    implement amazing git log with fzf
    inspiration: https://junegunn.kr/2015/03/browsing-git-commits-with-fzf/

--- fish/functions/gl.fish
+++ fish/functions/gl.fish
@@ -1,13 +1,33 @@
 function gl \
    -w "git log" \
-   -d "Show a nice git log"
+   -d "Show a nice git log searchable using fzf"
 
-    git log \
-        --graph \
-        --abbrev-commit \
-        --decorate \
-        --all \
-        --format=format:"%h%C(auto)%d %C(green)%cr %C(blue)%s %C(magenta)%an %C(reset)" \
-        $argv
+    set selected (
+
+        # display the git log
+        git log \
+            --graph \
+            --color=always \
+            --format=format:"%h%C(auto)%d %C(green)%cr %C(blue)%s %C(magenta)%an %C(reset)" \
+            $argv |
+
+        # and allow searching it with fzf
+        fzf \
+            --ansi \
+            --reverse \
+            --no-sort \
+            --multi \
+            --tiebreak=index \
+            --header="use ctrl-s to toggle sorting, enter to select a commit, tab to select multiple" \
+            --bind=ctrl-s:toggle-sort
+
+    )
+
+    # if anything was selected, print only the commit hashes
+    if count $selected > /dev/null
+        for s in $selected
+            echo $s | ag --only-matching "[a-f0-9]{7}" | head -1
+        end
+    end
 end
 
