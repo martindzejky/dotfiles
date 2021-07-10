e45c685edd920b04f1df97fa82a2dc030cea7ccc
Martin Jakubik <martin@jakubik.info>
2021-07-10 09:42:24 +0200

    install bat

---
+++ fish/config/colors.fish
@@ -0,0 +1,10 @@
+# COLORS for bat and delta
+# https://github.com/sharkdp/bat
+# https://github.com/dandavison/delta
+
+if is-os-dark-mode
+    set BAT_THEME "Solarized (dark)"
+else
+    set BAT_THEME "Solarized (light)"
+end
+
