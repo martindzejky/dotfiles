3765fdf17338bff6afacc5fe927b98f61ae4d42d
Martin Jakubik <martin@jakubik.info>
2022-04-01 23:03:05 +0200

    delete all dark mode related code since it no longer works in Hyper

--- fish/config/settings.fish
+++ fish/config/settings.fish
@@ -17,6 +17,3 @@ set -gx LANG en_US.UTF-8
 # gpg tty
 set -gx GPG_TTY (tty)
 
-# go modules
-set -gx GOPRIVATE gitlab.exponea.com
-
--- fish/functions/is-os-light-mode.fish
+++
@@ -1,9 +0,0 @@
-function is-os-light-mode \
-    -d "Checks whether the OS is in light mode"
-
-    # TODO
-    return 2
-
-    return (not is-os-dark-mode)
-end
-
